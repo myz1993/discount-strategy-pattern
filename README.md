916f409d43111652b9019a0f6419dc726d2a8f9c
Bo Zhang <bo@gradle.com>
2019-06-28 10:58:02 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Java面向对象初步练习：使用策略模式对打折系统进行重构
+
+请完成[`PriceCalculator`](https://github.com/hcsp/discount-strategy-pattern/blob/master/src/main/java/com/github/hcsp/polymorphism/)中的挑战，使用策略模式进行重构，使业务与打折策略分离。
+
+> 注：重构指的是在**不改变**原先代码功能的前提下，将原先的代码变得更精炼、可维护性更强。
+
+在提交Pull Request之前，你应当在本地确保所有代码已经编译通过，并且通过了测试(`mvn test`)
+
+-----
+注意！我们只允许你修改以下文件，对其他文件的修改会被拒绝：
+- [src/main/java/com/github/hcsp/polymorphism/](https://github.com/hcsp/discount-strategy-pattern/blob/master/src/main/java/com/github/hcsp/polymorphism/)
+-----
+
+
+完成题目有困难？不妨来看看[写代码啦的相应课程](https://xiedaimala.com/tasks/661cd7ab-7fea-47d0-8e11-555d6fca751d)吧！
+
+回到[写代码啦的题目](https://xiedaimala.com/tasks/661cd7ab-7fea-47d0-8e11-555d6fca751d/quizzes/6c87ef57-7f06-4af2-9112-86dd27ff099d)，继续挑战！
---
+++ src/test/java/com/github/hcsp/polymorphism/PriceCalculatorTest.java
@@ -0,0 +1,21 @@
+package com.github.hcsp.polymorphism;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PriceCalculatorTest {
+    @Test
+    public void test() {
+        Assertions.assertEquals(
+                95, PriceCalculator.calculatePrice(new Discount95Strategy(), 100, User.dios("屌丝")));
+        Assertions.assertEquals(
+                100,
+                PriceCalculator.calculatePrice(
+                        new OnlyVipDiscountStrategy(), 100, User.dios("屌丝")));
+        Assertions.assertEquals(
+                95,
+                PriceCalculator.calculatePrice(new OnlyVipDiscountStrategy(), 100, User.vip("土豪")));
+        Assertions.assertEquals(
+                100, PriceCalculator.calculatePrice(new NoDiscountStrategy(), 100, User.vip("土豪")));
+    }
+}
