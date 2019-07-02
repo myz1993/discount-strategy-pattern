2cfa6b2d61a3ceb9915fb1fe94cac2b826e90de6
Lingfeng <2354833@qq.com>
2019-07-02 20:33:01 +0800

    Discount

--- src/main/java/com/github/hcsp/polymorphism/DiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/DiscountStrategy.java
@@ -1,7 +1,6 @@
 package com.github.hcsp.polymorphism;
 
-public class DiscountStrategy {
-    public int discount(int price, User user) {
-        throw new UnsupportedOperationException();
-    }
+public  abstract class DiscountStrategy {
+    public abstract
+    int discount(int price, User user);
 }
