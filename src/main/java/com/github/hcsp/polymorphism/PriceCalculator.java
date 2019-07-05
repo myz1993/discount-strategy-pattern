54b29180f469dee76f46c7f91fac040f074b70ee
余韵之 <36914785+Lambert1129@users.noreply.github.com>
2019-07-05 09:38:40 +0800

    update4 (#8)
    update4 (#8)

--- src/main/java/com/github/hcsp/polymorphism/PriceCalculator.java
+++ src/main/java/com/github/hcsp/polymorphism/PriceCalculator.java
@@ -7,7 +7,12 @@ public class PriceCalculator {
     // OnlyVipDiscountStrategy 只有VIP打95折，其他人保持原价
     // 重构后的方法签名：
     // public static int calculatePrice(DiscountStrategy strategy, int price, User user)
+    public static int calculatePrice(DiscountStrategy strategy, int price, User user){
+        return strategy.discount(price,user);
+    }
+
     public static int calculatePrice(String discountStrategy, int price, User user) {
+
         switch (discountStrategy) {
             case "NoDiscount":
                 return price;
