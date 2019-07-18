25d0878bfd98c4717a450fe678d886be3963cbb6
zzxx9426 <mrxiao9426@gmail.com>
2019-07-18 17:27:23 +0800

    The strategy pattern (#17)
    The strategy pattern (#17)

--- src/main/java/com/github/hcsp/polymorphism/DiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/DiscountStrategy.java
@@ -3,5 +3,6 @@ package com.github.hcsp.polymorphism;
 public class DiscountStrategy {
     public int discount(int price, User user) {
         throw new UnsupportedOperationException();
+
     }
 }
