86c13cfe87e5b75df97bc5689c1c22bfa66b71d5
Ben <50296011+MaoYuanFang@users.noreply.github.com>
2019-07-27 13:28:30 +0800

    a1 (#22)
    a1 (#22)

--- src/main/java/com/github/hcsp/polymorphism/PriceCalculator.java
+++ src/main/java/com/github/hcsp/polymorphism/PriceCalculator.java
@@ -6,8 +6,9 @@ public class PriceCalculator {
     // Discount95Strategy 全场95折
     // OnlyVipDiscountStrategy 只有VIP打95折，其他人保持原价
     // 重构后的方法签名：
-    // public static int calculatePrice(DiscountStrategy strategy, int price, User user)
-    public static int calculatePrice(String discountStrategy, int price, User user) {
+    public static int calculatePrice(DiscountStrategy strategy, int price, User user){
+        return strategy.discount(price,user);
+    /*public static int calculatePrice(String discountStrategy, int price, User user) {
         switch (discountStrategy) {
             case "NoDiscount":
                 return price;
@@ -24,5 +25,7 @@ public class PriceCalculator {
             default:
                 throw new IllegalStateException("Should not be here!");
         }
+
+         */
     }
 }
