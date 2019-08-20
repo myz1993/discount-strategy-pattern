76650af09f825fd07d7e8d6b3398dac2e2d110a6
Suyu <zhuangsuyu1993@163.com>
2019-08-20 19:50:47 +0800

    打折策略模式 (#35)
    打折策略模式 (#35)

--- src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
@@ -1,3 +1,12 @@
 package com.github.hcsp.polymorphism;
 
-public class OnlyVipDiscountStrategy {}
+public class OnlyVipDiscountStrategy extends DiscountStrategy {
+    @Override
+    public int discount(int price, User user) {
+        if (user != null && user.isVip()){
+            return (int) (0.95 * price);
+        }
+
+        return price;
+    }
+}
