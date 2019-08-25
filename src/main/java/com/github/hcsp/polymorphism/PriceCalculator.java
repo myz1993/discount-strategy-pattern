86d836431fbb54b184eb355817812a874d8d2869
25127 <jie.wang13@hand-china.com>
2019-08-25 19:25:14 +0800

    多态的应用 策略模式

--- src/main/java/com/github/hcsp/polymorphism/PriceCalculator.java
+++ src/main/java/com/github/hcsp/polymorphism/PriceCalculator.java
@@ -1,6 +1,9 @@
 package com.github.hcsp.polymorphism;
 
 public class PriceCalculator {
+    public static int calculatePrice(DiscountStrategy strategy, int price, User user){
+        return strategy.discount(price, user);
+    }
     // 使用策略模式重构这个方法，实现三个策略：
     // NoDiscountStrategy 不打折
     // Discount95Strategy 全场95折
