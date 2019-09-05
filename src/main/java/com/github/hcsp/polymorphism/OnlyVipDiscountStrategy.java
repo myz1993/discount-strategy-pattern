1103bea804e7ce980673023abe443ee177348136
keliangyong <337215966@qq.com>
2019-09-05 18:10:57 +0800

    done (#43)
    done (#43)

--- src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
@@ -1,3 +1,8 @@
 package com.github.hcsp.polymorphism;
 
-public class OnlyVipDiscountStrategy {}
+public class OnlyVipDiscountStrategy extends DiscountStrategy {
+    @Override
+    public int discount(int price, User user) {
+        return user.isVip() ? 95:100;
+    }
+}
