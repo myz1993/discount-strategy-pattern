6c0ad0bcbe887d99058ca3599511d1bc2176017f
YakumoAoi <416910533@qq.com>
2019-09-09 15:09:01 +0800

    strategy_pattern (#44)
    strategy_pattern (#44)

--- src/main/java/com/github/hcsp/polymorphism/Discount95Strategy.java
+++ src/main/java/com/github/hcsp/polymorphism/Discount95Strategy.java
@@ -1,3 +1,9 @@
 package com.github.hcsp.polymorphism;
 
-public class Discount95Strategy {}
+public class Discount95Strategy extends DiscountStrategy{
+    final double rate=0.95;
+    @Override
+    public int discount(int price, User user) {
+        return (int)(price*rate);
+    }
+}
--- src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
@@ -1,3 +1,9 @@
 package com.github.hcsp.polymorphism;
 
-public class OnlyVipDiscountStrategy {}
+public class OnlyVipDiscountStrategy extends DiscountStrategy{
+    final double rate=0.95;
+    @Override
+    public int discount(int price, User user) {
+        return  (int)(price*(user.isVip()?rate:1));
+    }
+}
