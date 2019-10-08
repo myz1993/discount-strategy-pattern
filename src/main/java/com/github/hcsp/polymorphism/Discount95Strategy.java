352e1b0b49ba23418ab5dd10e72986c9680ee66d
wpsumsun <wpsumsun@126.com>
2019-10-08 20:51:00 +0800

    discount strategy pattern (#59)
    discount strategy pattern (#59)

--- src/main/java/com/github/hcsp/polymorphism/Discount95Strategy.java
+++ src/main/java/com/github/hcsp/polymorphism/Discount95Strategy.java
@@ -1,3 +1,10 @@
 package com.github.hcsp.polymorphism;
 
-public class Discount95Strategy {}
+import java.math.BigDecimal;
+
+public class Discount95Strategy extends DiscountStrategy{
+    @Override
+    public int discount(int price, User user) {
+        return (int) (price*0.95);
+    }
+}
