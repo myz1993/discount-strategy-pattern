f805f705ec8d7d642171a0decf49159b4f1c6158
ws1603 <18388866683@163.com>
2019-10-14 17:46:20 +0800

    使用策略模式对打折系统进行重构

--- src/main/java/com/github/hcsp/polymorphism/PriceCalculator.java
+++ src/main/java/com/github/hcsp/polymorphism/PriceCalculator.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.polymorphism;
 
 import com.sun.javaws.IconUtil;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class PriceCalculator {
     // 使用策略模式重构这个方法，实现三个策略：
@@ -9,7 +10,12 @@ public class PriceCalculator {
     // OnlyVipDiscountStrategy 只有VIP打95折，其他人保持原价
     // 重构后的方法签名：
     // public static int calculatePrice(DiscountStrategy strategy, int price, User user)
+
         public static int calculatePrice(DiscountStrategy strategy, int price, User user){
             return  strategy.discount(price,user);
         }
+
+    public static void main(String[] args) {
+        System.out.println(calculatePrice(new OnlyVipDiscountStrategy(),  100, User.vip("张三")));
+    }
 }
