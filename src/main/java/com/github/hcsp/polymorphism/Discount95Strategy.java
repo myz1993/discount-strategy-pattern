d0045467072d4ac9fe09cac76084aeb6a4859b62
monalisali <34482115+monalisali@users.noreply.github.com>
2019-12-11 21:45:42 +0800

    策略模式 (#84)
    策略模式 (#84)

--- src/main/java/com/github/hcsp/polymorphism/Discount95Strategy.java
+++ src/main/java/com/github/hcsp/polymorphism/Discount95Strategy.java
@@ -1,3 +1,8 @@
 package com.github.hcsp.polymorphism;
 
-public class Discount95Strategy {}
+public class Discount95Strategy extends DiscountStrategy {
+    @Override
+    public int discount(int price, User user) {
+        return (int) (price * 0.95);
+    }
+}
