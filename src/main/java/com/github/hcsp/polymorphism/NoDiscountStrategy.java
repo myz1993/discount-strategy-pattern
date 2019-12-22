1400cc0a07179103f499a55d59eccc8dd6c55fa5
Chenghao Zheng <45369893+NervousOrange@users.noreply.github.com>
2019-12-22 22:58:42 +0100

    from NervousOrange: Assignment 4 of Polymorphism. (#88)
    from NervousOrange: Assignment 4 of Polymorphism. (#88)

--- src/main/java/com/github/hcsp/polymorphism/NoDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/NoDiscountStrategy.java
@@ -1,3 +1,8 @@
 package com.github.hcsp.polymorphism;
 
-public class NoDiscountStrategy {}
+public class NoDiscountStrategy extends DiscountStrategy{
+    @Override
+    public int discount(int price, User user) {
+        return price;
+    }
+}
