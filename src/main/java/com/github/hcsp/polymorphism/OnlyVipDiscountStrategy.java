5638dda9ab01861ee3e2ca6ee48fb4d3f8df9b78
linsinan1995 <47880367+linsinan1995@users.noreply.github.com>
2019-12-24 03:30:28 +0800

    polymorphism design pattern. (#89)
    polymorphism design pattern. (#89)

--- src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
@@ -1,3 +1,16 @@
 package com.github.hcsp.polymorphism;
 
-public class OnlyVipDiscountStrategy {}
+public class OnlyVipDiscountStrategy extends DiscountStrategy {
+    public OnlyVipDiscountStrategy() {
+        super();
+    }
+
+    @Override
+    public int calculate(int price, User user) {
+        if (user.isVip()) {
+            return (int) (price * 0.95);
+        } else {
+            return price;
+        }
+    }
+}
