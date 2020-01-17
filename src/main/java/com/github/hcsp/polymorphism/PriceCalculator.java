f948f39b0879e9f659c3436560f8be13f197447a
yrucrew <1622077015@qq.com>
2020-01-18 00:07:00 +0800

    test (#109)
    test (#109)

--- src/main/java/com/github/hcsp/polymorphism/PriceCalculator.java
+++ src/main/java/com/github/hcsp/polymorphism/PriceCalculator.java
@@ -6,23 +6,25 @@ public class PriceCalculator {
     // Discount95Strategy 全场95折
     // OnlyVipDiscountStrategy 只有VIP打95折，其他人保持原价
     // 重构后的方法签名：
-    // public static int calculatePrice(DiscountStrategy strategy, int price, User user)
-    public static int calculatePrice(String discountStrategy, int price, User user) {
-        switch (discountStrategy) {
-            case "NoDiscount":
-                return price;
-            case "Discount95":
-                return (int) (price * 0.95);
-            case "OnlyVip":
-                {
-                    if (user.isVip()) {
-                        return (int) (price * 0.95);
-                    } else {
-                        return price;
-                    }
-                }
-            default:
-                throw new IllegalStateException("Should not be here!");
-        }
-    }
+     public static int calculatePrice(DiscountStrategy strategy, int price, User user) {
+         return strategy.discount(price, user);
+     }
+//    public static int calculatePrice(String discountStrategy, int price, User user) {
+//        switch (discountStrategy) {
+//            case "NoDiscount":
+//                return price;
+//            case "Discount95":
+//                return (int) (price * 0.95);
+//            case "OnlyVip":
+//                {
+//                    if (user.isVip()) {
+//                        return (int) (price * 0.95);
+//                    } else {
+//                        return price;
+//                    }
+//                }
+//            default:
+//                throw new IllegalStateException("Should not be here!");
+//        }
+//    }
 }
