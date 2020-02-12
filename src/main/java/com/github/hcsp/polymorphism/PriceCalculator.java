4e105115b98cca7cd7b472f01ccba169845bc6cf
yueyue chou <13444339+yueyuezone@users.noreply.github.com>
2020-02-12 18:35:32 +0800

    策略模式应用 (#117)
    策略模式应用 (#117)

--- src/main/java/com/github/hcsp/polymorphism/PriceCalculator.java
+++ src/main/java/com/github/hcsp/polymorphism/PriceCalculator.java
@@ -1,28 +1,16 @@
 package com.github.hcsp.polymorphism;
 
 public class PriceCalculator {
+    public static void main(String[] args) {
+        calculatePrice(new NoDiscountStrategy(),100,User.vip(""));
+    }
     // 使用策略模式重构这个方法，实现三个策略：
     // NoDiscountStrategy 不打折
     // Discount95Strategy 全场95折
     // OnlyVipDiscountStrategy 只有VIP打95折，其他人保持原价
     // 重构后的方法签名：
     // public static int calculatePrice(DiscountStrategy strategy, int price, User user)
-    public static int calculatePrice(String discountStrategy, int price, User user) {
-        switch (discountStrategy) {
-            case "NoDiscount":
-                return price;
-            case "Discount95":
-                return (int) (price * 0.95);
-            case "OnlyVip":
-                {
-                    if (user.isVip()) {
-                        return (int) (price * 0.95);
-                    } else {
-                        return price;
-                    }
-                }
-            default:
-                throw new IllegalStateException("Should not be here!");
-        }
+    public static int calculatePrice(DiscountStrategy discountStrategy, int price, User user) {
+      return discountStrategy.discount(price,user);
     }
 }
