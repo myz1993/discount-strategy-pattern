63287ed2ad64abb5ac6e776f3893becf8b9ed39d
KisetsuLee <42855253+KisetsuLee@users.noreply.github.com>
2020-02-25 11:41:19 +0800

    策略模式应用 (#123)
    策略模式应用 (#123)

--- src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
@@ -1,3 +1,8 @@
 package com.github.hcsp.polymorphism;
 
-public class OnlyVipDiscountStrategy {}
+public class OnlyVipDiscountStrategy extends DiscountStrategy {
+    @Override
+    public int discount(int price, User user) {
+        return user.isVip() ? (int) (price * 0.95) : price;
+    }
+}
