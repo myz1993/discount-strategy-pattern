0b948ac17f50c6b095bc8c2fba3794047289c815
硬核机器人 <49554844+hcsp-bot@users.noreply.github.com>
2020-03-05 20:23:40 +0800

    add code (#130)
    add code (#130)

--- src/main/java/com/github/hcsp/polymorphism/PriceCalculator.java
+++ src/main/java/com/github/hcsp/polymorphism/PriceCalculator.java
@@ -7,22 +7,7 @@ public class PriceCalculator {
     // OnlyVipDiscountStrategy 只有VIP打95折，其他人保持原价
     // 重构后的方法签名：
     // public static int calculatePrice(DiscountStrategy strategy, int price, User user)
-    public static int calculatePrice(String discountStrategy, int price, User user) {
-        switch (discountStrategy) {
-            case "NoDiscount":
-                return price;
-            case "Discount95":
-                return (int) (price * 0.95);
-            case "OnlyVip":
-                {
-                    if (user.isVip()) {
-                        return (int) (price * 0.95);
-                    } else {
-                        return price;
-                    }
-                }
-            default:
-                throw new IllegalStateException("Should not be here!");
-        }
+    public static int calculatePrice(DiscountStrategy strategy, int price, User user) {
+        return strategy.discount(price,user);
     }
 }
