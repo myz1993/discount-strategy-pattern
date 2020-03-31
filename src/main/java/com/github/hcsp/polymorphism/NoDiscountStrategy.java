530700a26482c4c520cd5ffd0c31cfc8f10bcc1c
JoeyPine <fanjiasong_7@outlook.com>
2020-04-01 00:00:39 +0800

    两个类类没有继承DiscountStrategy

--- src/main/java/com/github/hcsp/polymorphism/NoDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/NoDiscountStrategy.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.polymorphism;
 
 public class NoDiscountStrategy extends DiscountStrategy {
+    @Override
     public int discount(int price, User user) {
         return price;
     }
--- src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.polymorphism;
 
-public class OnlyVipDiscountStrategy {
+public class OnlyVipDiscountStrategy extends DiscountStrategy {
+    @Override
     public int discount(int price, User user) {
         if (user.isVip()) {
             return (int) (price * 0.95);
