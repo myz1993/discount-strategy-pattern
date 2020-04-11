6fedf271ec1a5f49209fa47e3e06b14e98e828c6
Ma Haoxiang <61976611+strivejojo@users.noreply.github.com>
2020-04-11 23:59:35 +0800

    Java面向对象初步练习：使用策略模式对打折系统进行重构 (#150)
    Java面向对象初步练习：使用策略模式对打折系统进行重构 (#150)

--- src/main/java/com/github/hcsp/polymorphism/DiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/DiscountStrategy.java
@@ -1,7 +1,8 @@
 package com.github.hcsp.polymorphism;
 
 public class DiscountStrategy {
+
     public int discount(int price, User user) {
-        throw new UnsupportedOperationException();
+        throw new IllegalStateException("Should not be here!");
     }
 }
