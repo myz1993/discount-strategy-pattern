9b02c08fa5a046c6aae9e5dd6322998c6d87c717
Run Ze Zheng <63222815+runzez-reyes@users.noreply.github.com>
2020-06-16 21:30:32 +0800

    discount strategy pattern (#170)
    discount strategy pattern (#170)

--- src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
@@ -1,3 +1,12 @@
 package com.github.hcsp.polymorphism;
 
-public class OnlyVipDiscountStrategy {}
+public class OnlyVipDiscountStrategy extends DiscountStrategy {
+    @Override
+    public int discount(int price, User user) {
+        if (user.isVip()) {
+            price *= 0.95;
+        }
+        return price;
+    }
+
+}
