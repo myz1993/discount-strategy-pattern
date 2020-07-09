68db74bd74593e9cf5aa9f16b5a76941ea4100e3
mushan <49012328+mushanwb@users.noreply.github.com>
2020-07-09 18:20:59 +0800

    策略模式重构代码 (#175)
    策略模式重构代码 (#175)

--- src/main/java/com/github/hcsp/polymorphism/Discount95Strategy.java
+++ src/main/java/com/github/hcsp/polymorphism/Discount95Strategy.java
@@ -1,3 +1,7 @@
 package com.github.hcsp.polymorphism;
 
-public class Discount95Strategy {}
+public class Discount95Strategy extends DiscountStrategy{
+    public int discount(int price, User user) {
+        return price * 95 / 100;
+    }
+}
--- src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
@@ -1,3 +1,11 @@
 package com.github.hcsp.polymorphism;
 
-public class OnlyVipDiscountStrategy {}
+public class OnlyVipDiscountStrategy extends DiscountStrategy{
+    public int discount(int price, User user) {
+        if (user.isVip()) {
+            return price * 95 / 100;
+        } else {
+            return price;
+        }
+    }
+}
