fa01ebf117570707b164d585174090cec2fbff6b
Yuanbin Jiang <58287154+bgzsqqwd123@users.noreply.github.com>
2020-11-13 14:42:53 +0800

    使用策略模式对打折系统进行重构 (#192)
    使用策略模式对打折系统进行重构 (#192)

--- src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
@@ -1,3 +1,12 @@
 package com.github.hcsp.polymorphism;
 
-public class OnlyVipDiscountStrategy {}
+public class OnlyVipDiscountStrategy extends DiscountStrategy {
+    @Override
+    public int discount(int price, User user) {
+        if (user.isVip()) {
+            return (int) (price * 0.95);
+        } else {
+            return price;
+        }
+    }
+}
