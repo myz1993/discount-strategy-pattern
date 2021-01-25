48d6cab67535c93dcb53ac4b111fa91fb5a1d5a0
2014 <suhanma@gmail.com>
2021-01-24 21:27:17 -0800

    discount (#201)
    discount (#201)

--- src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
@@ -1,3 +1,11 @@
 package com.github.hcsp.polymorphism;
 
-public class OnlyVipDiscountStrategy {}
+public class OnlyVipDiscountStrategy extends DiscountStrategy {
+    public int discount(int price, User user) {
+        if (user.isVip()) {
+            return (int) (price * 0.95);
+        } else {
+            return price;
+        }
+    }
+}
