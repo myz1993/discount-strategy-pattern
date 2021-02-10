2bab9a9be993011d9992e9aba214e2e5c6572dd5
Apolo Du <dudueasy@126.com>
2021-02-10 16:49:56 +0800

    update (#203)
    update (#203)

--- src/main/java/com/github/hcsp/polymorphism/NoDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/NoDiscountStrategy.java
@@ -1,3 +1,10 @@
 package com.github.hcsp.polymorphism;
 
-public class NoDiscountStrategy {}
+public class NoDiscountStrategy extends DiscountStrategy{
+
+    @Override
+    public int discount(DiscountStrategy strategy, int price, User user){
+        return price;
+    };
+
+}
