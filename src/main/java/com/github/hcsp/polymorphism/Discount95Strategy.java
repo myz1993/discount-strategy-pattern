9a8d9a9da5646e38299c14f44d00b4d1e09dad61
sltap <39816348+sltap@users.noreply.github.com>
2021-08-16 02:53:32 +0800

    13.1.4 (#213)
    13.1.4 (#213)

--- src/main/java/com/github/hcsp/polymorphism/Discount95Strategy.java
+++ src/main/java/com/github/hcsp/polymorphism/Discount95Strategy.java
@@ -1,3 +1,7 @@
 package com.github.hcsp.polymorphism;
 
-public class Discount95Strategy {}
+public class Discount95Strategy extends DiscountStrategy{
+    public int discount(int price,User user){
+        return (int)(0.95*price);
+    }
+}
